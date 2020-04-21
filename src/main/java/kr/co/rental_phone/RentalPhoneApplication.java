abefc374f0495c3ecdc9d9cc543659e341cfedae
devP0644 <phg0644@naver.com>
2020-04-21 15:55:59 +0900

    Initial commit

---
+++ src/main/java/kr/co/rental_phone/RentalPhoneApplication.java
@@ -0,0 +1,13 @@
+package kr.co.rental_phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalPhoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/rental_phone/RentalPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.rental_phone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalPhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
